e1e30dd8ec577c54d52acb4d2389a342b1a2e11a
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-29 10:48:50 +0200

    added needed imports for wtf forms in app/auth/forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+# from ..models import User
+from wtforms import ValidationError
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address', validators=[Required(), Email()])
