4a59ebdb524719a3b69c96d355fa8119bbe9d00d
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-29 11:07:54 +0200

    created a class that holds a form to add a pitch app/main/forms.py

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+
+
+class PitchForm(FlaskForm):
+
+    title = StringField('Pitch title', validators=[Required()])
+    text = TextAreaField('Text', validators=[Required()])
+    category = SelectField('Type', choices=[('business', 'Business pitch'), (
+        'promotion', 'Promotion pitch'), ('product', 'Product pitch')], validators=[Required()])
+    submit = SubmitField('Create pitch')
