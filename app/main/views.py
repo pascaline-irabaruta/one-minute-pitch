19e2c6873d53dc453e3a70e7632293ce7b61f5a9
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-29 11:24:50 +0200

    created a function to save the picture of the user in app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -28,3 +28,13 @@ def update_profile(uname):
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('.profile', uname=user.username))
+@main.route('/user/<uname>/update/pic', methods=['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username=uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile', uname=uname))
