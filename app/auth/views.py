f9787ca6c9ab71946bd5c358d1282c7322824e4c
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-29 10:55:33 +0200

    add imports in app/auth/views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,10 @@
-
+from flask import render_template,redirect,url_for, flash,request
+from flask_login import login_user,logout_user,login_required
+from ..models import User
+from .forms import LoginForm,RegistrationForm
+from .. import db
+from . import auth
+from ..email import mail_message
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
