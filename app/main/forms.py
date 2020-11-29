c2746262f2995130a3ebe9b07834d5df4e5752d6
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-29 11:09:30 +0200

    created a class that holds a form to update boi app/main/forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,7 @@ class PitchForm(FlaskForm):
     category = SelectField('Type', choices=[('business', 'Business pitch'), (
         'promotion', 'Promotion pitch'), ('product', 'Product pitch')], validators=[Required()])
     submit = SubmitField('Create pitch')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Bio.', validators=[Required()])
+    submit = SubmitField('Submit')
