8c8ff527e0610207489fd33d5bdb8f6ce28870d3
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-29 12:54:23 +0200

    save chamges

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,11 @@
-
+from flask import render_template, request, redirect, url_for, abort
+from . import main
+from flask_login import login_required, current_user
+from ..models import User, Pitch, Comment
+from .. import photos, db
+from datetime import datetime
+# import DateTime
+from .forms import PitchForm, CommentForm
 
 @main.route('/')
 def index():
