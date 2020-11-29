5a1195a1c8c1454ad1eb2c17a73f3bf1f62e0c31
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-29 11:20:41 +0200

    created a function to filter the user by username in app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -8,3 +8,11 @@ def index():
     promotion_pitches = Pitch.get_pitches('promotion')
     product_pitches = Pitch.get_pitches('product')
     return render_template('index.html', title=title, business=business_pitches, promotion=promotion_pitches, product=product_pitches)
+@main.route('/profile/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    pitches_count = Pitch.count_pitches(uname)
+    user_joined = user.date_joined.strftime('%b %d, %Y')
+    if user is None:
+        abort(404)
+    return render_template("profile/profile.html", user=user, pitches=pitches_count, date=user_joined)
