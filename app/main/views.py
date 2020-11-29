b75a7837e7fb628a5bd2258786b750752a55b0bd
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-29 11:22:24 +0200

    created a function to save the updated user boi in app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -16,3 +16,15 @@ def profile(uname):
     if user is None:
         abort(404)
     return render_template("profile/profile.html", user=user, pitches=pitches_count, date=user_joined)
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('.profile', uname=user.username))
