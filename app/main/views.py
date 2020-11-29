53cb237d1646e34c6a5ee34e35f0cd43ce8994a4
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-29 11:34:36 +0200

    created a function to add likes,dislikes and comment to a pitch in app/main/views.py

--- app/main/views.py
+++ app/main/views.py
@@ -79,3 +79,38 @@ def product_pitches():
     pitches = Pitch.get_pitches('product')
 
     return render_template("product_pitches.html", pitches=pitches)
+
+
+@main.route('/pitch/<int:id>', methods=['GET', 'POST'])
+def pitch(id):
+    pitch = Pitch.get_pitch(id)
+    posted_date = pitch.posted.strftime('%b %d, %Y')
+
+    if request.args.get("like"):
+        pitch.likes = pitch.likes + 1
+
+        db.session.add(pitch)
+        db.session.commit()
+
+        return redirect("/pitch/{pitch_id}".format(pitch_id=pitch.id))
+
+    elif request.args.get("dislike"):
+        pitch.dislikes = pitch.dislikes + 1
+
+        db.session.add(pitch)
+        db.session.commit()
+
+        return redirect("/pitch/{pitch_id}".format(pitch_id=pitch.id))
+
+    comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        comment = comment_form.text.data
+
+        new_comment = Comment(
+            comment=comment, user=current_user, pitch_id=pitch)
+
+        new_comment.save_comment()
+
+    comments = Comment.get_comments(pitch)
+
+    return render_template("pitch.html", pitch=pitch, comment_form=comment_form, comments=comments, date=posted_date)
