1a5030db432e139907a8ed06b8b94669ea904a69
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2020-11-30 09:48:56 +0200

    deployment to heroku

--- config.py
+++ config.py
@@ -23,8 +23,8 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://pascy:21338@:5432/pt'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     '''
